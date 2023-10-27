29f587a12036345abd7f7b03da158fa1a7b6cce6
shan5742 <asamshan456@gmail.com>
2023-10-27 22:52:49 +0100

    [bugfix] - fixing plugins

--- src/server/api/routers/svg.ts
+++ src/server/api/routers/svg.ts
@@ -1,5 +1,8 @@
 import { z } from "zod";
 import { transform } from '@svgr/core';
+import svgoPlugin from '@svgr/plugin-svgo';
+import jsxPlugin from "@svgr/plugin-jsx";
+import prettierPlugin from "@svgr/plugin-prettier";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { ExportType } from "~/types";
 
@@ -15,7 +18,7 @@ export const svgRouter = createTRPCRouter({
     const jsxOutput = await transform(
       svgString,
       {
-        plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx', '@svgr/plugin-prettier'],
+        plugins: [svgoPlugin, jsxPlugin, prettierPlugin],
         icon: true,
         exportType: exportType,
       },
